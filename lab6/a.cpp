66245b20458d311446625a1530e9a6bd8e7a8c55
qorganbek <dimashqorganbek@gmail.com>
2022-11-03 22:32:57 +0600

    upd

---
+++ lab6/a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int partition(string &a, int low, int high){
+    int i = low - 1;
+    int pivot = high;
+    for(int j = low; j <= high - 1; ++j){
+        if(a[j] < a[pivot]){
+            i++;
+            char save2 = a[i];
+            a[i] = a[j];
+            a[j] = save2;
+        }
+    }
+    char save = a[pivot];
+    a[pivot] = a[i + 1];
+    a[i + 1] = save;
+    return i + 1;
+}
+
+
+void qsort(string &a, int low, int high){
+    if(low < high){
+        int pivot = partition(a, low, high);
+        qsort(a, low, pivot - 1);
+        qsort(a, pivot + 1, high);
+    }
+}
+
+int main(){
+
+    return 0;
+}
---
+++ lab6/qs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+ 
+
+// void qsort(int *a,int low, int high){
+//     if (low < high){
+
+//     }
+// }
+
+
+int main (){
+    int a[] = {1, 15, 6, 8, -8, 7};
+    int n = sizeof(a) / sizeof(int);
+
+    return 0;
+}
