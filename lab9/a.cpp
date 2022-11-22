ecc4e2bfd9467eb7cad8d981008bf37290568f81
qorganbek <dimashqorganbek@gmail.com>
2022-11-22 16:12:13 +0600

    upd

--- lab9/a.cpp
+++ lab9/a.cpp
@@ -1,50 +1,55 @@
 #include <bits/stdc++.h>
 
-#define ll long long
-
 using namespace std;
-size_t ind = 0;
-void f(string t, string s){
-    size_t n = t.size();
-    size_t m = s.size();
 
-    ll h[n];
-    ll p[n];
-    ll q = LONG_LONG_MAX;
 
-    p[0] = 1;
-
-    for(size_t i = 1; i < max(n, m); ++i){
-        p[i] = (p[i - 1] * 31) % q;
-    }
-
-    for(size_t i = 0; i < n; ++i){
-        h[i] = ((t[i] - int('a') + 1) * p[i]) % q;
-        if(i > 0){
-            h[i] = (h[i] + h[i - 1])  % q;
+vector<int> kmp(string s){
+    int n = s.size();
+    vector<int> v(n);
+    for(int i = 1; i < n; i++){
+        int j = v[i-1];
+        while (j > 0 && s[j] != s[i])
+        {
+            j = v[j-1];
+        }
+        if(s[i] == s[j]){
+            v[i] = j + 1;
         }
     }
-    ll h_s = 0;
-    for(size_t i = 0; i < m; ++i){
-        h_s = (h_s + ((s[i] - int('a') + 1) * p[i]) % q) % q;
-    }
+    return v;
+}
 
-    for(size_t i = 0; i + m - 1 < n; i++){
-        ll d = h[i + m - 1];
-        if(i > 0){
-            d -= h[i-1];
-        }
-        if( d == h_s * p[i]  && t.substr(i, m) == s){
-            ind = i;
-        }
-    }
 
+bool check(string s, string x){
+    vector<int> v = kmp(x + '#' + s);
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] == x.size()) return true;
+    }
+    return false;
 }
 
 int main(){
-    string text, pattern;
-    cin >> text >> pattern;
-    f(text, pattern);
-    cout << ind;
+    string s, x;
+    cin >> s >> x;
+    int cnt = 1;
+    string temp = s;
+
+    while (x.size() > s.size())
+    {
+        cnt++;
+        s += temp;
+    }
+    if(check(s,x)){
+        cout << cnt;
+        return 0;
+    }
+    else {
+        s += temp;
+        if(check(s,x)){
+            cout << cnt+1;
+            return 0;
+        }   
+    }
+    cout << -1;
     return 0;
 }
